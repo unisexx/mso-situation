0eb02c93e98b2ebc32b3ae9de0ed2fbef071f904
unisexx <unisexx@hotmail.com>
2015-10-14 13:51:47 +0700

    ooooo

--- application/modules/books/controllers/books.php
+++ application/modules/books/controllers/books.php
@@ -28,11 +28,14 @@ class Books extends Public_Controller
 	
 	function view2(){
 		$book = new Book();
-		if(@$_GET['category']){ $book->where('category_id = '.$_GET['category']); }
-		$data['books'] = $book->where('category_id = 9 or category_id = 8 and status = "approve"')->order_by('orderlist','desc')->get();
+		if(@$_GET['category']){
+			$data['books'] = $book->where('category_id = '.$_GET['category'])->order_by('orderlist','desc')->get();
+		}else{
+			$data['books'] = $book->where('category_id = 9 or category_id = 8 or category_id = 11 or category_id = 12 and status = "approve"')->order_by('orderlist','desc')->get();
+		}
 		
 		$category = new Category();
-		$data['categories'] = $category->where('id = 9 and id = 8')->order_by('name asc')->get();
+		$data['categories'] = $category->where('id = 9 or id = 8 or id = 11 or id = 12')->order_by('name asc')->get();
 		$this->load->view('view2',$data);
 	}
 	
